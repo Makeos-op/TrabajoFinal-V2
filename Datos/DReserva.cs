88b1c5c8d7591727ea693cc78e0cd2ac2fc7303b
Sebastian Medina <smedinamayo@gmail.com>
2025-12-05 14:39:14 -0500

    Agregado de reserva

--- Datos/DReserva.cs
+++ Datos/DReserva.cs
@@ -6,8 +6,55 @@ using System.Threading.Tasks;
 
 namespace Datos
 {
-    public class DReserva
+    public class DReserva:AccesoBD
     {
-        //Insertar algo
+        //Registro
+        public string Registro(Reserva b)
+        {
+            if (b?.Vehiculo==null || b?.Espacio== null)
+            {
+                return "El vehiculo y/o reserva no puede ser nulo.";
+            }
+            return EjecutarFuncion(bd =>
+            {
+                bd.Reserva.Add(b);
+                bd.SaveChanges();
+                return "Reserva registrado correctamente.";
+            });
+        }
+        //Modificar
+        public string Modificar(Reserva b)
+        {
+            if (b?.Vehiculo == null || b?.Espacio == null)
+            {
+                return "El vehiculo y/o reserva no puede ser nulo.";
+            }
+            return EjecutarFuncion(bd =>
+            {
+                var reserva = bd.Reserva.Find(b.IdReserva);
+                if (reserva == null)
+                {
+                    return "Reserva no encontrada.";
+                }
+                bd.Entry(reserva).CurrentValues.SetValues(b);
+                bd.SaveChanges();
+                return "Reserva modificado correctamente.";
+            });
+        }
+        //Eliminar
+        public string Eliminar(int idReserva)
+        {
+            return EjecutarFuncion(bd =>
+            {
+                var reserva = bd.Reserva.Find(idReserva);
+                if (reserva == null)
+                {
+                    return "Reserva no encontrada.";
+                }
+                bd.Reserva.Remove(reserva);
+                bd.SaveChanges();
+                return "Reserva eliminada correctamente.";
+            });
+        }
     }
 }
